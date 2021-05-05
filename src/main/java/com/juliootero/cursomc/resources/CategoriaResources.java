51734820e5fb899c767756ab01ad70d996acba2d
juliop110 <juliootero7@gmail.com>
2021-05-05 15:48:31 -0300

    commit inicial

--- src/main/java/com/juliootero/cursomc/resources/CategoriaResources.java
+++ src/main/java/com/juliootero/cursomc/resources/CategoriaResources.java
@@ -6,12 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(value = "/categorias")
 class CategoriaResource {
     @RequestMapping(method=RequestMethod.GET)
-    public List<Categoria> listar() {
-        Categoria cat1 = new Categoria(1, "Informática");
-        Categoria cat2 = new Categoria(2, "Escritório");
-        List<Categoria> lista = new ArrayList<>();
-        lista.add(cat1);
-        lista.add(cat2);
-        return lista;
+    public String listar() {
+        return "REST está funcionando!";
     }
 }

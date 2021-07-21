d7a5b290c73de95f04e27a44e48662f2bf2e8d47
juliop110 <juliootero7@gmail.com>
2021-07-21 14:43:29 -0300

    Busca de produtos por nome e categorias

--- src/main/java/com/juliootero/cursomc/services/PedidoService.java
+++ src/main/java/com/juliootero/cursomc/services/PedidoService.java
@@ -2,6 +2,7 @@ package com.juliootero.cursomc.services;
 
 
 import com.juliootero.cursomc.domain.Pedido;
+import com.juliootero.cursomc.domain.Produto;
 import com.juliootero.cursomc.repositories.PedidoRepository;
 import com.juliootero.cursomc.services.exceptions.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,12 +11,12 @@ import org.springframework.stereotype.Service;
 import java.util.Optional;
 
 @Service
-public class PedidoService {
+public class ProdutoService {
 
     @Autowired
     private PedidoRepository repo;
 
-    public Pedido find(Integer id) {
+    public Produto find(Integer id) {
         Optional<Pedido> obj = repo.findById(id);
         return obj.orElseThrow(() -> new  ObjectNotFoundException(
                 "Objeto não encontrado! Id: " + id + ", Tipo: " + Pedido.class.getName()));
